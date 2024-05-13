98a7fab3487370d07ca4e5d61d650e22e3142bd1
Nguyễn Đăng Nam <ndnam4922@gmail.com>
2024-05-13 22:28:51 +0700

    reinitialized repo

---
+++ events-platform-ui/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="bg-blue-500 text-white p-4">
+        Welcome to React with Tailwind CSS!
+      </header>
+    </div>
+  );
+}
+
+export default App;
