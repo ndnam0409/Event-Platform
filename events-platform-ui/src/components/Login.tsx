8796db06d6a341591fe1546846b64d4b6b4e3116
skandavivek2 <skanda.vivek@gmail.com>
2024-05-19 11:56:54 +0700

    implement login & signup

--- events-platform-ui/src/components/Login.tsx
+++ events-platform-ui/src/components/Login.tsx
@@ -1,119 +1,40 @@
-import { Fragment } from "react/jsx-runtime";
-import '@fortawesome/fontawesome-free/css/all.css';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook,faGoogle,faCentos } from '@fortawesome/free-brands-svg-icons';
-import { useState } from "react";
-const SocialLoginButton = () => (
-	<Fragment>
-		<button className="bg-blue-600 text-white py-3 px-6 rounded w-full flex items-center justify-center mt-4">
-			<FontAwesomeIcon icon={faFacebook} className=" mr-2 text-white" />
-			<span className="text-center">Continue with Facebook</span>
-		</button>
-		<button className="bg-red-500 text-white py-3 px-6 rounded w-full flex items-center justify-center mt-4">
-			<FontAwesomeIcon icon={faGoogle} className=" mr-2 text-white" />
-			<span className="text-center">Continue with Google</span>
-		</button>
-	</Fragment>
-);
-
-const SignInForm = () => {
-	const [validated, setValidated] = useState(false);
-
-	const handleSubmit = (event:React.FormEvent<HTMLFormElement>) => {
-		event.preventDefault();
-
-		const form = event.currentTarget;
-		if (form.checkValidity() === false) {
-			event.preventDefault();
-			event.stopPropagation();
-		}
-
-		setValidated(true);
-	};
-
-	return (
-		<form noValidate={validated} onSubmit={handleSubmit}>
-			<div className="mb-4">
-				<label className="block mb-2 font-normal" htmlFor="email">
-					Email Address
-				</label>
-				<input
-					type="text"
-					className="w-full bg-blue-50 dark:bg-slate-700 min-h-[48px] leading-10 px-4 p-2 rounded-lg outline-none border border-transparent focus:border-blue-600"
-					id="email"
-					placeholder="Enter Email Address"
-				/>
-			</div>
-			<div className="mb-4">
-				<label className="block mb-2 font-normal" htmlFor="password">
-					Password
-				</label>
-				<input
-					type="password"
-					className="w-full bg-blue-50 dark:bg-slate-700 min-h-[48px] leading-10 px-4 p-2 rounded-lg outline-none border border-transparent focus:border-blue-600"
-					id="password"
-					placeholder="Enter Password"
-				/>
-			</div>
-			<div className="mb-4">
-				<input type="checkbox" className="mr-2" id="remember-me" value=""/>
-				<label className="font-normal" htmlFor="remember-me">
-					Remember me
-				</label>
-			</div>
-			<button className="bg-indigo-900 text-white py-3 px-6 rounded w-full">
-				Log In
-			</button>
-			<button className="hover:text-blue-600 py-2 px-4 rounded-lg w-full">
-				Forget your password?
-			</button>
-			<div className="relative">
-				<hr className="my-8 border-t border-gray-300" />
-				<span className="px-2 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white dark:bg-[#0b1727]">
-					Or
-				</span>
-			</div>
-			<SocialLoginButton />
-		</form>
-	);
-};
-
 const Login = () => {
-	return (
-		<section className="ezy__signin6_udDNaPwh py-14 md:py-24 bg-white dark:bg-[#0b1727] text-indigo-900 dark:text-white">
-			<div className="container px-4 mx-auto">
-				<div className="grid grid-cols-12 gap-6 lg:gap-16 h-full">
-					<div className="col-span-12 lg:col-span-5">
-						<div
-							className="bg-center bg-no-repeat bg-cover w-full min-h-[150px] rounded-[25px] hidden lg:block h-full"
-							style={{
-								backgroundImage:
-									"url(public/Event1.png)",
-							}}
-						></div>
-					</div>
-					<div className="col-span-12 lg:col-span-5 py-12 lg:ml-20">
-						<div className="flex items-center justify-center max-w-lg h-full">
-							<div className="w-full mx-auto">
-								<h2 className="text-indigo-900 dark:text-white text-2xl font-bold mb-3">
-									Welcome to EventPlatform
-								</h2>
-								<div className="flex items-center mb-6 md:mb-12">
-									<p className="mb-0 mr-2 opacity-50">Don't have an account?</p>
-									<a href="#!" className="">
-										Create Account
-									</a>
-								</div>
+  return (
+    <div className=" flex justify-center bg-gradient-to-r min-h-screen items-center">
+      <div className=" w-1/4 min-w-96 px-10 py-8 rounded-2xl border-solid border-white border-2">
+        <form action="">
+          <h1 className=" text-4xl text-center text-white">Login</h1>
+          <div className=" w-full h-12 my-7">
+            <input className=" w-full h-full bg-transparent outline-none border-solid border-white border-2
+             rounded-xl placeholder:text-white
+              text-white px-6
+              " type="text" placeholder="Username" required />
+          </div>
+          <div className="w-full h-12 my-7">
+            <input  className=" w-full h-full bg-transparent outline-none border-solid border-white 
+            border-2 border-r rounded-xl
+            placeholder:text-white px-6 text-white" type="password" placeholder="Password" required />
+          </div>
+          <div className="flex justify-center mx-auto
+           border-white border-2 bg-pink-600 border-none
+            h-10 rounded-xl text-white
+             
+           ">
+          <button type="submit" className=" text-white w-11/12">
+            Login
+          </button>
+          </div>
+          <div className=" text-white flex-col justify-between">
+            <p>
+              Don't have account?  <a href="">Register</a>
+            </p>
+            <a className=" text-white" href="#">Forgot password?</a>
+          </div>
+          <div></div>
+        </form>
+      </div>
+    </div>
+  )
+}
 
-								<SignInForm />
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
-};
-
-export default Login;
+export default Login
--- events-platform-ui/src/components/Signup.tsx
+++ events-platform-ui/src/components/Signup.tsx
@@ -1,261 +1,44 @@
-import { Fragment, useState } from "react";
-import '@fortawesome/fontawesome-free/css/all.css';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook,faGoogle,faCentos } from '@fortawesome/free-brands-svg-icons';
-
-const monthList = [
-	{ label: "January", value: 1 },
-	{ label: "February", value: 2 },
-	{ label: "March", value: 3 },
-	{ label: "April", value: 4 },
-	{ label: "May", value: 5 },
-	{ label: "June", value: 6 },
-	{ label: "July", value: 7 },
-	{ label: "August", value: 8 },
-	{ label: "September", value: 9 },
-	{ label: "October", value: 10 },
-	{ label: "November", value: 11 },
-	{ label: "December", value: 12 },
-];
-const dayList = [
-	1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-	23, 24, 25, 26, 27, 28, 29, 30, 31,
-];
-// Get the current year
-const currentYear = new Date().getFullYear();
-
-// Create an array to store the years
-let years = [];
-
-// Loop from 1900 to the current year and push each year as a string into the array
-for (let year = 1900; year <= currentYear; year++) {
-    years.push(year.toString());
-}
-
-
-const yearList=years
-
-const SocialLoginButton = () => (
-	<Fragment>
-		<button className="w-full flex justify-center items-center bg-blue-600 rounded py-3 px-5 mb-3">
-			<FontAwesomeIcon icon={faFacebook} className="text-white text-2xl" />
-			<span className="w-full text-center text-white">
-				Continue with Facebook
-			</span>
-		</button>
-		<button className="w-full flex justify-center items-center bg-red-500 rounded py-3 px-5 mb-3">
-			<FontAwesomeIcon icon={faGoogle} className="text-white text-2xl" />
-			<span className="w-full text-center text-white">
-				Continue with Google
-			</span>
-		</button>
-	</Fragment>
-);
-
-const SignUpForm = () => {
-	const [validated, setValidated] = useState(false);
-
-	const handleSubmit = (event:React.FormEvent<HTMLFormElement>) => {
-		event.preventDefault();
-
-		const form = event.currentTarget;
-		if (form.checkValidity() === false) {
-			event.preventDefault();
-			event.stopPropagation();
-		}
-
-		setValidated(true);
-	};
-
-	return (
-		<form noValidate={validated} onSubmit={handleSubmit}>
-			<div className="flex flex-wrap">
-				<div className="w-full lg:w-1/2">
-					<div className="flex flex-col mb-6 mx-2">
-						<label htmlFor="first-name" className="mb-2">
-							First Name
-						</label>
-						<input
-							type="text"
-							className="bg-blue-50 dark:bg-slate-700 dark:bg-opacity-50 rounded-xl min-h-[54px] leading-10 px-4 focus:outline-none focus:bg-blue-100 dark:focus:bg-opacity-100"
-							id="first-name"
-							placeholder="Your First Name"
-						/>
-					</div>
-				</div>
-				<div className="w-full lg:w-1/2">
-					<div className="flex flex-col mb-6 mx-2">
-						<label htmlFor="last-name" className="mb-2">
-							Last Name
-						</label>
-						<input
-							type="text"
-							className="bg-blue-50 dark:bg-slate-700 dark:bg-opacity-50 rounded-xl min-h-[54px] leading-10 px-4 focus:outline-none focus:bg-blue-100 dark:focus:bg-opacity-100"
-							id="last-name"
-							placeholder="Your Last Name"
-						/>
-					</div>
-				</div>
-				<div className="w-full">
-					<div className="mb-6">
-						<label htmlFor="birth-date" className="mb-2">
-							Birth date
-						</label>
-						<div className="w-full flex rounded-xl overflow-hidden">
-							<select
-							
-                                typeof="day"
-								className="min-h-[54px] leading-10 bg-blue-50 dark:bg-slate-700 dark:bg-opacity-50 focus:outline-none focus:bg-blue-100 dark:focus:bg-opacity-100 border-r border-gray-300 dark:border-gray-700 w-1/3 px-4"
-							>
-								<option hidden defaultValue="01">
-									Day
-								</option>
-								{dayList.map((day, i) => (
-									<option value={day} key={i}>
-										{day}
-									</option>
-								))}
-							</select>
-							<select
-                            
-								typeof="month"
-								className="min-h-[54px] leading-10 bg-blue-50 dark:bg-slate-700 dark:bg-opacity-50 focus:outline-none focus:bg-blue-100 dark:focus:bg-opacity-100 border-r border-gray-300 dark:border-gray-700 w-1/3 px-4"
-							>
-								<option hidden defaultValue="01">
-									Month
-								</option>
-								{monthList.map(({ label, value }, i) => (
-									<option value={value} key={i}>
-										{label}
-									</option>
-								))}
-							</select>
-							<select
-								typeof="year"
-								className="min-h-[54px] leading-10 bg-blue-50 dark:bg-slate-700 dark:bg-opacity-50 focus:outline-none focus:bg-blue-100 dark:focus:bg-opacity-100 w-1/3 px-4"
-							>
-								<option hidden defaultValue="1990">
-									Year
-								</option>
-                                {yearList.map((day, i) => (
-									<option value={day} key={i}>
-										{day}
-									</option>
-								))}
-							</select>
-						</div>
-					</div>
-				</div>
-				<div className="w-full">
-					<div className="flex flex-col mb-6 mx-2">
-						<label htmlFor="email" className="mb-2">
-							Email
-						</label>
-						<input
-							type="email"
-							className="bg-blue-50 dark:bg-slate-700 dark:bg-opacity-50 rounded-xl min-h-[54px] leading-10 px-4 focus:outline-none focus:bg-blue-100 dark:focus:bg-opacity-100"
-							id="email"
-							placeholder="Email"
-						/>
-					</div>
-				</div>
-				<div className="w-full lg:w-1/2">
-					<div className="flex flex-col mb-6 mx-2">
-						<label htmlFor="password" className="mb-2">
-							Password
-						</label>
-						<input
-							type="password"
-							className="bg-blue-50 dark:bg-slate-700 dark:bg-opacity-50 rounded-xl min-h-[54px] leading-10 px-4 focus:outline-none focus:bg-blue-100 dark:focus:bg-opacity-100"
-							id="password"
-							placeholder="Password"
-						/>
-					</div>
-				</div>
-				<div className="w-full lg:w-1/2">
-					<div className="flex flex-col mb-6 mx-2">
-						<label htmlFor="con-pass" className="mb-2">
-							Confirm Password
-						</label>
-						<input
-							type="text"
-							className="bg-blue-50 dark:bg-slate-700 dark:bg-opacity-50 rounded-xl min-h-[54px] leading-10 px-4 focus:outline-none focus:bg-blue-100 dark:focus:bg-opacity-100"
-							id="con-pass"
-							placeholder="Confirm Password"
-						/>
-					</div>
-				</div>
-			</div>
-			<div className="mb-6">
-				<div className="form-check">
-					<input className="rounded-lg" type="checkbox" value="" id="agree" />
-					<label className="rounded-lg" htmlFor="agree">
-						{" "}
-						I accept to the{" "}
-						<a href="#!" className="underline">
-							terms & condition
-						</a>
-						,
-						<a href="#!" className="underline">
-							privacy policy
-						</a>
-					</label>
-				</div>
-			</div>
-			<button
-				type="submit"
-				className="bg-indigo-800 text-white px-7 py-3 rounded w-full"
-			>
-				Sign Up
-			</button>
-
-			<div className="relative">
-				<hr className="my-6 md:my-12" />
-				<span className="px-2 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white dark:bg-[#0b1727]">
-					Or
-				</span>
-			</div>
-
-			<SocialLoginButton />
-		</form>
-	);
-};
-
 const SignUp = () => {
 	return (
-		<section className="ezy__signup6_9YyLfTBD py-14 md:py-24 bg-white dark:bg-[#0b1727] text-zinc-900 dark:text-white">
-			<div className="container px-4 mx-auto">
-				<div className="grid grid-cols-12 h-full">
-					<div className="col-span-12 lg:col-span-6">
-						<div
-							className="bg-center bg-no-repeat bg-cover w-full min-h-[150px] rounded-[25px] hidden lg:block h-full"
-							style={{
-								backgroundImage:
-									"url(public/Event1.png)",
-							}}
-						></div>
-					</div>
-					<div className="col-span-12 lg:col-span-5 lg:col-start-8 py-12">
-						<div className="flex items-center justify-center h-full">
-							<div className="w-full max-w-2xl mx-auto">
-								<h2 className="text-indigo-900 dark:text-white text-3xl font-bold mb-3">
-									Welcome to EventPlatform
-								</h2>
-								<div className="flex items-center mb-6 md:mb-12">
-									<p className="mb-0 mr-2 opacity-50">
-										Already have an account?
-									</p>
-									<a href="#!">Sign In</a>
-								</div>
-
-								<SignUpForm />
-							</div>
-						</div>
+	 <div className="flex flex-col justify-center items-center bg-gradient-to-r min-h-screen">
+		<div>
+			<h1 className="text-4xl text-center text-white">Create an account</h1>
+			<p className="text-center text-white">Already have an account? <a href="#">Log in</a></p>
+		</div>
+		<div className=" mt-8  w-1/3 min-w-96 px-10 py-8 rounded-2xl ">
+			<form action="" >
+				<div className="text-white">
+					<label className="block" htmlFor="">What should we call you</label>
+					<input className="h-10 block w-full bg-transparent border-solid  border-2
+					rounded-lg border-slate-300
+					 px-6 text-white" type="text" placeholder="Enter your profile name" />
+				</div>
+				<div className="mt-5 text-white">
+					<label className="block" htmlFor="">What should we call you</label>
+					<input className=" h-10 block w-full bg-transparent border-solid  border-2
+					rounded-lg border-slate-300
+					 px-6 text-white" type="text" placeholder="Enter your email address" />
+				</div>
+				<div className="mt-5 text-white">
+					<label className="block" htmlFor="">What should we call you</label>
+					<input className="h-10 block w-full bg-transparent border-solid border-2
+					rounded-lg border-slate-300
+					 px-6 text-white" type="text" placeholder="Enter your password" />
+					<p>Use 8 or more characters with a mix of letters, numbers</p>
+				</div>
+				<div className="mt-6 text-white">
+					<p>By creating an account, you agree to the Terms of use and Privacy Policy.</p>
+					<div className="flex justify-center mx-auto
+           border-white border-2 bg-pink-600 border-none
+            h-10 rounded-xl text-white">
+						<button>Create an account</button>
 					</div>
 				</div>
-			</div>
-		</section>
-	);
-};
-export default SignUp;
+			</form>
+		</div>
+		<div></div>
+	 </div>
+	)
+  }
+  
+  export default SignUp
