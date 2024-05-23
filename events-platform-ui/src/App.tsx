c728959907a223bf2a898d1ae87bccd823c24d16
Nguyễn Đăng Nam <ndnam4922@gmail.com>
2024-05-23 21:44:06 +0700

    update help-center images directory

--- events-platform-ui/src/App.tsx
+++ events-platform-ui/src/App.tsx
@@ -1,27 +1,37 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Blog from "./components/Blog";
 import Brand from "./components/Brand";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Login from "./components/Login";
-import SignUp from "./components/Signup";
-import Navbar from './components/Navbar';
-import UpcomingEvent from './components/UpcomingEvent';
-import PageNotFound from './components/PageNotFound';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HelpCenter from "./components/HelpCenter";
+import Login from "./components/Login";
+import Navbar from "./components/Navbar";
+import PageNotFound from "./components/PageNotFound";
+import SignUp from "./components/Signup";
+import UpcomingEvent from "./components/UpcomingEvent";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-    <Route path="/" element={<><Navbar/><Header/><UpcomingEvent/></>}>
-    </Route>
-    <Route path="login" element={<Login/>}/>
-    <Route path="signup" element={<SignUp/>}/>
-    <Route path="help-center" element={<HelpCenter/>}/>
-    <Route path="*" element={<PageNotFound />} />
-
-    </Routes>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Navbar />
+              <Header />
+              <UpcomingEvent />
+              <Brand />
+              <Blog />
+              <Footer />
+            </>
+          }
+        ></Route>
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="help-center" element={<HelpCenter />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </BrowserRouter>
   );
 }
